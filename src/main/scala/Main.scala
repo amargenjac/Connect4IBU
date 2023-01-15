f15538dffa015a2882deae9bd455b5c3034f437c
Amar Genjac <amar.genjac@stu.ibu.edu.ba>
2023-01-15 19:27:00 +0100

    added dynamic board size

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,19 @@
+import scala.util.control.Breaks._
 object Connect4 {
-  val rows = 6
-  val cols = 7
   val win = 4
   val empty = '.'
+  println("Please input dimensions of the board")
+  println("-------------------------------------")
+  val rows = getRows()
+  val cols = getCols()
+
+  //check for invalid board size needs to be implemented here, loop until the size is good
 
   val board = Array.fill(rows, cols)(empty)
  
  
   def main(args: Array[String]): Unit = {
     var player = 'O'
-   
     while (true) {
       for (row <- board) {
       for (cell <- row) {
@@ -26,6 +30,34 @@ object Connect4 {
         player = if (player == 'X') 'O' else 'X'
   }
 }
+}
+
+// check if the board is too small or if the difference between rows and cols is greater than 2
+def isValidSize(row: Int, col: Int): Boolean ={
+  if (row < 6 || col < 7){
+    println("Minimum size of the board should be 6x7. Please change your inputs")
+    false
+  }
+  else if(row - col >= 2 || rows - cols <= -3){
+    println("Difference between rows and columns can't be greater than 2")
+    false
+  }
+  true
+
+}
+
+//helper function to input rows
+def getRows(): Int ={
+  println("Please input number of rows you want to have in your game.")
+  val rows = scala.io.StdIn.readInt()
+  rows
+}
+
+//helper function to input cols
+def getCols(): Int ={
+  println("Please input number of columns you want to have in your game.")
+  val cols = scala.io.StdIn.readInt()
+  cols
 }
 
 
